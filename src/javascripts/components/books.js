78a29bff7ff0586c3779ebc051c42d6f61d65146
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-02-18 15:37:54 -0600

    updated dom events

--- src/javascripts/components/books.js
+++ src/javascripts/components/books.js
@@ -10,6 +10,7 @@ const makeBooks = (array) => {
         <h5 class="card-title">${item.title}</h5>
         <p class="card-text bold">${item.sale ? `<span class="badge badge-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i> Sale</span> $${item.price}` : `$${item.price}`}</p>
         <hr>
+        <button class="btn btn-info" id="edit-book--${item.firebaseKey}">Edit Book</button>
         <button class="btn btn-danger" id="delete-book--${item.firebaseKey}">Delete Book</button>
         </div>
       </div>`;
--- src/javascripts/events/domEvents.js
+++ src/javascripts/events/domEvents.js
@@ -1,15 +1,19 @@
-// THIS FILE IS FOR ANYTHING DYNAMIC THAT WE NEED TO LISTEN FOR ON THE DOM
 const domEvents = () => {
   document.querySelector('body').addEventListener('click', (e) => {
-  // CLICK EVENT FOR DELETING A BOOK
+    // CLICK EVENT FOR DELETING A BOOK
     if (e.target.id.includes('delete-book')) {
       if (window.confirm('Want to delete?')) {
         console.warn('CLICKED DELETE BOOK', e.target.id);
       }
     }
-  });
 
-  // CLICK EVENT FOR DELETING AN AUTHOR
+    // CLICK EVENT FOR EDITING A BOOK
+    if (e.target.id.includes('edit-book')) {
+      console.warn('CLICKED EDIT BOOK', e.target.id);
+    }
+
+  // ADDCLICK EVENT FOR DELETING AN AUTHOR
+  });
 };
 
 export default domEvents;
